51c536ec4b31c64de4dbb16b083fe66fdc6e1c95
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-06-15 14:02:53 -0500

    Proyecto landing page animaciones

---
+++ animaciones-transiciones/landing-page-animation/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+window.addEventListener("load", () => {
+  const modal = document.querySelector(".modal");
+  const button1 = document.getElementById("button1");
+  const button2 = document.getElementById("button3");
+  const button3 = document.getElementById("button2");
+  const close = document.querySelector(".modal__content--close");
+  button1.addEventListener("click", () => {
+    aparecer();
+  });
+  button2.addEventListener("click", () => {
+    aparecer();
+  });
+  button3.addEventListener("click", () => {
+    aparecer();
+  });
+  close.addEventListener("click", () => {
+    desaparecer();
+  });
+
+  function aparecer() {
+    modal.classList.remove("hidden");
+    modal.classList.add("visible");
+  }
+
+  function desaparecer() {
+    modal.classList.remove("visible");
+    modal.classList.add("hidden");
+  }
+});
