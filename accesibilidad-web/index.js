bbef5132f3a7daf84052b5543750ed51f63a9473
Mateo Álvarez <maalvarezmu@unal.edu.co>
2024-01-16 13:01:54 -0500

    Ultimos tips para accesibilidad web

--- accesibilidad-web/index.js
+++ accesibilidad-web/index.js
@@ -3,11 +3,12 @@ window.onload = () => {
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
   document
     .querySelector(".send-button")
-    .addEventListener("click", showNotification);
+    .addEventListener("click", (e) => validateForm(e));
   document.querySelectorAll(".project").forEach((element) => {
     element.addEventListener("click", (e) => openModal(e));
   });
   document.body.addEventListener("click", (e) => closeModal(e));
+  document.body.addEventListener("keyup", (e) => listenForEsc(e));
 };
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
@@ -22,6 +23,30 @@ function clickRight() {
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch (newValue) {
+    case -270:
+      document.querySelector(".project1").setAttribute("tabindex", "-1");
+      document
+        .querySelector("project1-container")
+        .setAttribute("aria-hidden", "true");
+      document.querySelector(".project4").removeAttribute("tabindex");
+      document
+        .querySelector("project4-container")
+        .removeAttribute("aria-hidden");
+      break;
+    case -540:
+      document.querySelector(".project2").setAttribute("tabindex", "-1");
+      document
+        .querySelector("project2-container")
+        .setAttribute("aria-hidden", "true");
+      document.querySelector(".project5").removeAttribute("tabindex");
+      document
+        .querySelector("project5-container")
+        .removeAttribute("aria-hidden");
+      break;
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
@@ -36,19 +61,80 @@ function clickLeft() {
   }
   let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch (newValue) {
+    case -270:
+      document.querySelector(".project5").setAttribute("tabindex", "-1");
+      document
+        .querySelector("project5-container")
+        .setAttribute("aria-hidden", "true");
+      document.querySelector(".project2").removeAttribute("tabindex");
+      document
+        .querySelector("project2-container")
+        .removeAttribute("aria-hidden");
+      break;
+    case 0:
+      document.querySelector(".project4").setAttribute("tabindex", "-1");
+      document
+        .querySelector("project4-container")
+        .setAttribute("aria-hidden", "true");
+      document.querySelector(".project1").removeAttribute("tabindex");
+      document
+        .querySelector("project1-container")
+        .removeAttribute("aria-hidden");
+      break;
+    default:
+      break;
+  }
+}
+
+function validateForm(e) {
+  e.preventDefault();
+  const name = document.getElementById("nombre").value;
+  const email = document.getElementById("correo").value;
+  const message = document.getElementById("mensaje").value;
+  document.getElementById("nameError").innerHTML = "";
+  document.getElementById("emailError").innerHTML = "";
+  document.getElementById("messageError").innerHTML = "";
+  if (name === "") {
+    document.getElementById("nameError").innerHTML =
+      "Por favor ingrese su nombre";
+    return;
+  }
+  if (email === "") {
+    document.getElementById("emailError").innerHTML =
+      "Por favor ingrese su email";
+    return;
+  }
+  if (message === "") {
+    document.getElementById("messageError").innerHTML =
+      "Por favor ingrese un mensaje";
+    return;
+  }
+  showNotification();
 }
 
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
+  document.querySelector(".notification").innerHTML =
+    "El formulario fue enviado sin errores";
+  document.querySelector(".form-container").reset();
   document.querySelector(".notification").style.display = "flex";
   setTimeout(function () {
     document.querySelector(".notification").style.display = "none";
   }, 3000);
+  document.querySelector(".notification").innerHTML = "";
+}
+
+function listenForEsc(e) {
+  if (e.keyCode === 27) {
+    closeModal(e);
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
   document.querySelector(".modal-container").style.display = "flex";
+  document.getElementById("modal-header").focus();
 }
 
 /** Esta funcion se llama para cerrar el modal */
--- accesibilidad-web/tips.md
+++ accesibilidad-web/tips.md
@@ -1,7 +1,7 @@
 # Trucos para mejorar la accesibilidad
 
 1. Usar la herramienta de chrome para simular las discapacidades visuales.
-2. Navegar manualmente por la pagina con el teclado.
+2. Navegar manualmente por la pagina con el teclado usando la tecla tab y shift + tab para ir hacia atras.
 3. Los unicos elementos que pueden tener el foco son los botones, enlaces, campos de texto, checkbox, radio button, select, textarea, etc.
 4. Usar lectores de pantalla.
 5. Usar HTML semantico.
@@ -60,3 +60,18 @@
 ```
 
 12. Al usar hover tambien usar focus para los usuarios que navegan con el teclado. (En algunos casos es bueno usar css :focus-within)
+13. Usar tabindex para darle foco a elementos que no son enlaces, botones, checkbox, radio button, select, textarea, etc.
+
+- Valor negativo(-1): El elemento debe ser enfocado, pero no debe de ser accesible a través de la navegación.
+
+- Valor positivo (>0): Debe poder ser enfocado y su orden relativo es definido por el valor del atributo.
+
+- Valor de 0: Debe ser enfocado y ser accesible a través de la navegación secuencial del teclado, pero su orden relativo es definido por convención de la plataforma.
+
+14. Usar aria-live para indicar que un elemento cambia dinamicamente, por ejemplo: un mensaje de error por un input faltante en un formulario, o un mensaje de confirmación.
+
+- Off: Es el estado predeterminado.
+
+- Polite: Espera a que el lector de pantalla termine de leer el elemento actual. Se utiliza cuando el mensaje que se quiere comunicar, no es importante
+
+- Assertive: Interrumpe la lectura para dar el mensaje. Se utiliza cuando se quiere dar prioridad al mensaje, por ejemplo, en el mensaje de confirmación del envío de un formulario .
